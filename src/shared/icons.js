5bf39f1003800f9221c81120cf9018ae25bcbba4
Tomi Toshev <toshevtomi@gmail.com>
2026-09-25 09:44:54 +0300

    Dropped the monitor icon, which nothing draws now.

--- src/shared/icons.js
+++ src/shared/icons.js
@@ -24,10 +24,6 @@ const ICONS = {
   moon: `
     <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
   `,
-  monitor: `
-    <rect x="2" y="3" width="20" height="14" rx="2" />
-    <path d="M8 21h8M12 17v4" />
-  `,
 };
 
 /**
